abee18d8c3e2c3bd732a8d678a89412607a15699
Ivan Petrychenko <ivanpetrychenko95@gmail.com>
2025-03-20 18:55:04 +0000

    ceramic step 7 was added

---
+++ Ceramic_step_7/src/js/script.js
@@ -0,0 +1,33 @@
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "/src/sass/style.scss";
+
+try {
+	new Swiper(".works__slider", {
+		slidesPerView: 1,
+		loop: true,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		navigation: {
+			nextEl: ".icon-right-open",
+			prevEl: ".icon-left-open",
+		},
+		breakpoints: {
+			// when window width is >= 1200px
+			1200: {
+				slidesPerView: 3,
+				spaceBetween: 5,
+			},
+			1920: {
+				spaceBetween: 35,
+			},
+		},
+		modules: [Navigation, Pagination],
+	});
+} catch (e) {}
