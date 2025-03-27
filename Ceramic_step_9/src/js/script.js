71d1e338e3f2df0d6a07bcc325d69f8576df55b8
Ivan Petrychenko <ivanpetrychenko95@gmail.com>
2025-03-27 21:53:55 +0000

    ceramic step 9 was added

---
+++ Ceramic_step_9/src/js/script.js
@@ -0,0 +1,47 @@
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "/src/sass/style.scss";
+
+const burger = document.querySelector(".burger"),
+	close = document.querySelector(".header__menu-close"),
+	menu = document.querySelector(".header__menu");
+
+burger.addEventListener("click", () => {
+	menu.classList.add("header__menu_active");
+	document.body.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+	menu.classList.remove("header__menu_active");
+	document.body.style.overflow = "";
+});
+
+try {
+	new Swiper(".works__slider", {
+		slidesPerView: 1,
+		loop: true,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		navigation: {
+			nextEl: ".icon-right-open",
+			prevEl: ".icon-left-open",
+		},
+		breakpoints: {
+			// when window width is >= 1200px
+			1200: {
+				slidesPerView: 3,
+				spaceBetween: 5,
+			},
+			1920: {
+				spaceBetween: 35,
+			},
+		},
+		modules: [Navigation, Pagination],
+	});
+} catch (e) {}
