cdfa22ef77f00356778c14df46f395fcbc0bc923
Ivan Petrychenko <ivanpetrychenko95@gmail.com>
2025-06-01 13:34:41 +0100

    ceramic step 16 was added

---
+++ Ceramic_step_16/vite.config.js
@@ -0,0 +1,48 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import viteImagemin from "vite-plugin-imagemin";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				catalog: "catalog.html",
+				blog: "blog.html",
+				about: "about.html",
+			},
+		},
+	},
+	plugins: [
+		viteImagemin({
+			gifsicle: {
+				optimizationLevel: 7,
+				interlaced: false,
+			},
+			optipng: {
+				optimizationLevel: 7,
+			},
+			mozjpeg: {
+				quality: 70,
+			},
+			pngquant: {
+				quality: [0.8, 0.9],
+				speed: 4,
+			},
+			svgo: {
+				plugins: [
+					{
+						name: "removeViewBox",
+					},
+					{
+						name: "removeEmptyAttrs",
+						active: false,
+					},
+				],
+			},
+		}),
+	],
+});
